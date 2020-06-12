088d16783226e412a835b3d65c1fa31420e714f6
Jerry Liu <zliu633@uwo.ca>
2020-06-11 22:44:54 -0400

    added pull to refresh
    known issues:
    1.after  refresh to pull, still need to switch between pages to get the home page updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,17 @@
+
 import 'package:demo/AssessmentPage.dart';
 import 'package:demo/MailPage.dart';
 import 'package:demo/MePage.dart';
+import 'package:demo/main.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:demo/modules/httpconnector.dart';
 void main() {
   runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   static const String _title = 'demo';
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -21,8 +24,8 @@ class MyApp extends StatelessWidget {
 class Home extends StatefulWidget {
   //boolean health from Submitted widget
   final bool isHealth;
-
-  Home({Key key,this.isHealth}) : super (key: key);
+  String value;
+  Home({Key key,this.isHealth,}) : super (key: key);
 
   @override
   _HomeState createState() => _HomeState();
@@ -32,7 +35,18 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   String titleName = "Assessment";
   static var healthStatus = 'Unknown';
+  bool isExpire= false;
 
+  Future<void> checkStatus() async {
+    return await post_initpage('5ee2e3fb05121809388ee81a').then((String value) => {
+    print("isExpire in checkstatus: "+ isExpire.toString()),
+      if(value == 'expire'){
+        setState((){
+          isExpire = true;
+        })
+      }
+    } );
+  }
   List createList() {
     //Check if user has submitted an assessment
     if(widget.isHealth != null){//If there is data
@@ -42,10 +56,12 @@ class _HomeState extends State<Home> {
     var _page = [new AssessmentPage(healthStatus: healthStatus),
       new mailPage(),
       new MePage()];
+    print("isExpire in createlist: "+ isExpire.toString());
+    if(isExpire){
+      _page[0] = new AssessmentPage(healthStatus: 'Unknown');
+    }
     return _page;
   }
-  //List of pages from bottom navigation bar
-  var _page = [new AssessmentPage(),new mailPage(),new MePage()];
 
   //Tapped on bottom navigation bar
   void _onItemTapped(int index) {
@@ -55,10 +71,27 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void initState() {
+    // TODO: implement setState
+  super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body:createList()[_selectedIndex],
+
+      body:RefreshIndicator(
+        onRefresh: checkStatus,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: Container(
+              child: createList()[_selectedIndex],
+              height: MediaQuery.of(context).size.height,
+          ),
+        ),
+
+        //Stack(children: createList()[_selectedIndex])
+      ),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
--- lib/modules/httpconnector.dart
+++ lib/modules/httpconnector.dart
@@ -1,1 +1,13 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+Future<String> post_initpage(String userId) async{
+  var data = {
+    'user_id': userId
+  };
+  var response = await http.post("http://ec2-54-160-79-156.compute-1.amazonaws.com:8080/checkexp",
+      body: json.encode(data), headers: {"Content-Type": "application/json"});
+
+  return response.body;
+}
